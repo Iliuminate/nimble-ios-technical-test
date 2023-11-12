77591dfe02872a9496204173fd0fa43d9d28174c
Carlos Diaz <carlosdz.centi@gmail.com>
2023-11-12 13:51:15 -0500

    Init Project with workspace

---
+++ nimble-ios-technical-test/nimble-ios-technical-test/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  nimble-ios-technical-test
+//
+//  Created by CarlosDz on 12/11/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ nimble-ios-technical-test/nimble-ios-technical-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nimble-ios-technical-test
+//
+//  Created by CarlosDz on 12/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
