dbfa6fa21745c9f0605d76f798334f269b403297
Carlos Diaz <carlosdz.centi@gmail.com>
2023-11-12 16:36:49 -0500

    Define base of clean architecture

--- nimble-ios-technical-test/nimble-ios-technical-test/AppDelegate.swift
+++ nimble-ios-technical-test/nimble-ios-technical-test/AppDelegate.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = MainController()
+        let navigation = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigation
+        self.window = window
+        window.makeKeyAndVisible()
+        
         return true
+        
     }
 }
